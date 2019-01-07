7b53d6c831423083ec8d291de8cb79aee34cc9f7
LammLora <lele.cecchegmail.com>
2019-01-07 18:22:42 +0100

    boh

--- js/Var.js
+++ js/Var.js
@@ -13,8 +13,8 @@ var playerJump = -400;
 var slowDownFactor = 0.87;
 var bulletVelocity = 500;
 
-var spawnX = 91*m;
-var spawnY = 47*m;
+var spawnX = 1*m;
+var spawnY = 57*m;
 
 //WOLF var
 var wolfJump = -250;
