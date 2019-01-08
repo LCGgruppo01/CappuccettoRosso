9ef46cebe26f5427d0b6e4997d85bcdfd75119bf
Aquets <37075016+Aquets@users.noreply.github.com>
2019-01-08 16:57:29 +0100

    scena

--- js/Functions.js
+++ js/Functions.js
@@ -353,12 +353,12 @@ function wolfKingShot(){
 
     bone.tempo = Math.sqrt(Math.pow((kingWolf.x - playerUp.x), 2) + Math.pow((kingWolf.y - playerUp.y), 2))/500;
 
-    bone.body.velocity.x = -500;
-    bone.body.velocity.y = -Math.sqrt(Math.pow((kingWolf.x - playerUp.x), 2) + 10*Math.pow((kingWolf.y - playerUp.y), 2))/1.5; //(Math.sqrt(Math.pow(500, 2) - Math.pow(bone.body.velocity.x, 2)) -0.5*gravity*bone.tempo);
+    bone.body.velocity.x = -(kingWolf.x - playerUp.x)/bone.tempo;
+    bone.body.velocity.y = (Math.sqrt(Math.pow(500, 2) - Math.pow(bone.body.velocity.x, 2)) -0.5*gravity*bone.tempo);
     kingShot = game.time.now + 2000*Math.random();
 
     xt.text = 'x ' + bone.body.velocity.x;
-    yt.text = 'y ' + bone.body.velocity.y;
+    yt.text = 'y ' + borderTop.cameraOffset.y;
     tempo.text = 'tempo ' + bone.tempo;
     velocità.text = 'velocità ' + bone.tempo*500/bone.tempo;
   }
