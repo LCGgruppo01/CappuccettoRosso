9d2bbcb3ef609760dea428f4462f3d17e21268a1
LammLora <lele.cecchegmail.com>
2019-01-08 16:50:53 +0100

    bloodmoon

---
+++ js/Bloodmoon.js
@@ -0,0 +1,19 @@
+var ScenaBloodmoon = {
+
+  preload: function() {
+    game.load.crossOrigin = 'anonymous';
+    game.load.image('scena', 'assets/scene/bloodmoon.jpg');
+  },
+
+  create: function() {
+    scena = game.add.sprite(0, 0, 'scena');
+    scena.events.onInputUp.add(imageClick2);
+  },
+
+};
+
+function imageClick2() {
+    this.game.state.start('GameLevel_1');
+};
+
+game.state.add('ScenaBloodmoon', ScenaBloodmoon);
