93a18f71d4420a8274b958fc72db244d18c2e61e
EmanueleCeccherini <37079906+EmanueleCeccherini@users.noreply.github.com>
2019-01-20 16:55:27 +0100

    start corretto

--- js/Var.js
+++ js/Var.js
@@ -12,8 +12,8 @@ var playerVelocity = 400;
 var playerJump = -450;
 var slowDownFactor = 0.87;
 var bulletVelocity = 500;
-var spawnX = 90*m;
-var spawnY = 47*m;
+var spawnX = 2*m;
+var spawnY = 52*m;
 
 //WOLF var
 var wolfJump = -250;
